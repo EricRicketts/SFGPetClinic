351f34337969e1be51c449a24e83c88865697dab
Eric Ricketts <eric_ricketts@me.com>
2022-06-12 18:00:55 -0400

    Veterinarian index page and controller

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VeterinarianController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VeterinarianController {
+
+    @RequestMapping({"/veterinarians", "/veterinarians/index", "/veterinarians/index.html"})
+    public String listVeterinarians() {
+        return "veterinarians/index";
+    }
+}
