7813575e6b9e15f4c99cd6e95f25d72707971db3
Eric Ricketts <eric_ricketts@me.com>
2022-06-13 05:23:56 -0400

    create owner index page and controller closes #12

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/veterinarians")
+@RequestMapping("/owners")
 @Controller
-public class VeterinarianController {
+public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVeterinarians() {
-        return "veterinarians/index";
+    public String listOwners() {
+        return "owners/index";
     }
 }
