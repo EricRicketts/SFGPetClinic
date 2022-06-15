c6e3a12906de9f6754c070be39953b78fb3b534b
Eric Ricketts <eric_ricketts@me.com>
2022-06-15 11:48:43 -0400

    add tests to prove data is being loaded into the targeted maps

--- sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ sfg-pet-clinic/pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,16 +1,39 @@
 package guru.springframework.sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VeterinarianService;
+import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
+	VeterinarianService veterinarianService;
+	OwnerService ownerService;
+
+	@BeforeEach
+	public void setUp() {
+		ownerService = context.getBean(OwnerServiceMap.class);
+		veterinarianService = context.getBean(VeterinarianService.class);
+	}
 	@Test
-	void contextLoads() {
+	void testOwnerService() {
+		assertEquals(2, ownerService.findAll().size());
+	}
+
+	@Test
+	void testVeterinarianService() {
+		assertEquals(2, veterinarianService.findAll().size());
 	}
 
 }
